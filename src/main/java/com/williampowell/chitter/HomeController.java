864f956f561bc8cfb3f8830050035a4d84a265db
William Powell <william.powell92@gmail.com>
2018-06-17 13:46:23 +0100

    test drove homecontroller

--- src/main/java/com/williampowell/chitter/HomeController.java
+++ src/main/java/com/williampowell/chitter/HomeController.java
@@ -1,7 +1,16 @@
 package com.williampowell.chitter;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+        return "index";
+    }
+
 }
+
+
--- src/test/java/com/williampowell/chitter/HomeControllerTest.java
+++ src/test/java/com/williampowell/chitter/HomeControllerTest.java
@@ -1,7 +1,23 @@
 package com.williampowell.chitter;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(SpringRunner.class)
 public class HomeControllerTest {
+
+    @MockBean
+    private HomeController homeController;
+
+    @Test
+    public void runsIndex(){
+        when(homeController.index()).thenCallRealMethod();
+        assertEquals("index", homeController.index());
+    }
 
 }
